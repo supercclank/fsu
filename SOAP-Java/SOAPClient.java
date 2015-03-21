869872d71f8c95bbc3f3615e2faceaa6d77776f7
Edward Yu <edwardyu236@gmail.com>
2015-03-21 05:58:05 -0400

    Edward's attempt at making a soap client
    currently only handles soap responses…? code actually untested since
    Edward has no clue

--- SOAP-Java/SOAPClient.java
+++ SOAP-Java/SOAPClient.java
@@ -1,6 +1,17 @@
 // SOAP Client created by Edward Sum Lok Yu
+// Trying to base off of SOAPClientExample.java
 import javax.xml.soap.*;
 
 public class SOAPClient {
 
+    public static void HandleSOAPResponse(SOAPRequest soapRequest, String soapServerURL) throws Exception {
+        SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+        SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+
+        SOAPMessage soapResponse = soapConnection.call(soapRequest, soapServerURL);
+        System.out.print("Response SOAP Message: ");
+        soapMessage.writeTo(System.out);
+
+        soapConnection.close();
+    }
 }
